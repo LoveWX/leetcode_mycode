f51c573676bde815eed71a9f8aa32763af2a2649
LoveWX <ztf0025@gmail.com>
2015-05-04 22:55:21 +0800

    Update 160_Intersection of Two Linked Lists.cpp

--- sniper_cpp/160_Intersection of Two Linked Lists.cpp
+++ sniper_cpp/160_Intersection of Two Linked Lists.cpp
@@ -10,49 +10,16 @@ class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
     {
-        if(headA==NULL || headB==NULL)
+        ListNode *pa=headA,*pb=headB;
+        if(pa==NULL || pb==NULL)
         {
             return NULL;
         }
-        int la=0,lb=0;
-        ListNode *p=headA,*q=headB;
-        while(p!=NULL)
+        while(pa!=pb)
         {
-            la++;
-            p=p->next;
+            pa = pa==NULL ? headB : pa->next;
+            pb = pb==NULL ? headA : pb->next;
         }
-        while(q!=NULL)
-        {
-            lb++;
-            q=q->next;
-        }
-        p=headA;
-        q=headB;
-        if(la>lb)
-        {
-            la-=lb;
-            while(la--)
-            {
-                p=p->next;
-            }
-        }
-        else
-        {
-            lb-=la;
-            while(lb--)
-            {
-                q=q->next;
-            }
-        }
-        while(p!=NULL && q!=NULL)
-        {
-            if(p==q)
-            {
-                return p;
-            }
-            p=p->next;
-            q=q->next;
-        }
-        return NULL;
+        return pa;
     }
 };
