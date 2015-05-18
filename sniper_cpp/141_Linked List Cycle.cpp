25e6bb859286e9e858e3e4e6b18bd3b3f1f8bf27
LoveWX <ztf0025@gmail.com>
2015-05-19 00:47:55 +0800

    Create 141_Linked List Cycle.cpp

---
+++ sniper_cpp/141_Linked List Cycle.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head)
+    {
+        ListNode *p=head,*q=head;
+        while(q!=NULL)
+        {
+            p=p->next;
+            q=q->next;
+            if(q==NULL)
+            {
+                return false;
+            }
+            q=q->next;
+            if(p==q)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
